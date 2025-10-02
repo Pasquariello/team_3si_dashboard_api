f0ca56c92083e414e0f527c0c4418fd400993575
kerj <kerntzj@gmail.com>
2025-10-02 16:35:44 -0700

    comment out unused util

--- src/controllers/providerData.ts
+++ src/controllers/providerData.ts
@@ -535,85 +535,85 @@ export async function getProviderMonthData(req: express.Request, res: express.Re
   }
 }
 
-async function getProviderAnnualDataUtility(yearNum: number) {
-  if (Number.isNaN(yearNum) || yearNum < 1980 || yearNum > 2100) {
-    // return res.status(400).json({ error: "Invalid year parameter" });
-  }
+// async function getProviderAnnualDataUtility(yearNum: number) {
+//   if (Number.isNaN(yearNum) || yearNum < 1980 || yearNum > 2100) {
+//     // return res.status(400).json({ error: "Invalid year parameter" });
+//   }
 
-  const sqlQuery = `
-    WITH combined AS (
-      SELECT
-        COALESCE(b.provider_licensing_id, p.provider_licensing_id, d.provider_licensing_id, s.provider_licensing_id) AS provider_licensing_id,
-        COALESCE(b.total_billed_over_capacity, 0) AS total_billed_over_capacity,
-        COALESCE(p.total_placed_over_capacity, 0) AS total_placed_over_capacity,
-        COALESCE(d.total_distance_traveled, 0) AS total_distance_traveled,
-        COALESCE(s.total_same_address, 0) AS total_same_address,
+//   const sqlQuery = `
+//     WITH combined AS (
+//       SELECT
+//         COALESCE(b.provider_licensing_id, p.provider_licensing_id, d.provider_licensing_id, s.provider_licensing_id) AS provider_licensing_id,
+//         COALESCE(b.total_billed_over_capacity, 0) AS total_billed_over_capacity,
+//         COALESCE(p.total_placed_over_capacity, 0) AS total_placed_over_capacity,
+//         COALESCE(d.total_distance_traveled, 0) AS total_distance_traveled,
+//         COALESCE(s.total_same_address, 0) AS total_same_address,
 
-        COALESCE(b.total_billed_over_capacity, 0) +
-        COALESCE(p.total_placed_over_capacity, 0) +
-        COALESCE(d.total_distance_traveled, 0) +
-        COALESCE(s.total_same_address, 0) AS overall_risk_score
-      FROM (
-        SELECT provider_licensing_id,
-          SUM(CASE WHEN billed_over_capacity_flag THEN 1 ELSE 0 END) AS total_billed_over_capacity    
-        FROM cusp_audit.demo.monthly_billed_over_capacity
-        WHERE YEAR(CAST(StartOfMonth AS DATE)) = ${yearNum}
-        GROUP BY provider_licensing_id
-      ) b
-      FULL OUTER JOIN (
-        SELECT provider_licensing_id, 
-          SUM(CASE WHEN placed_over_capacity_flag THEN 1 ELSE 0 END) AS total_placed_over_capacity    
-        FROM cusp_audit.demo.monthly_placed_over_capacity
-        WHERE YEAR(CAST(StartOfMonth AS DATE)) = ${yearNum}
-        GROUP BY provider_licensing_id
-      ) p
-        ON b.provider_licensing_id = p.provider_licensing_id
-      FULL OUTER JOIN (
-        SELECT provider_licensing_id, 
-          SUM(CASE WHEN distance_traveled_flag THEN 1 ELSE 0 END) AS total_distance_traveled   
-        FROM cusp_audit.demo.monthly_distance_traveled
-        WHERE YEAR(CAST(StartOfMonth AS DATE)) = ${yearNum}
-        GROUP BY provider_licensing_id
-      ) d
-        ON COALESCE(b.provider_licensing_id, p.provider_licensing_id) = d.provider_licensing_id
-      FULL OUTER JOIN (
-        SELECT provider_licensing_id, 
-          SUM(CASE WHEN same_address_flag THEN 1 ELSE 0 END) AS total_same_address      
-        FROM cusp_audit.demo.monthly_providers_with_same_address
-        WHERE YEAR(CAST(StartOfMonth AS DATE)) = ${yearNum}
-        GROUP BY provider_licensing_id
-      ) s
-        ON COALESCE(b.provider_licensing_id, p.provider_licensing_id, d.provider_licensing_id) = s.provider_licensing_id
-    )
-    SELECT
-      c.provider_licensing_id,
-      pa.provider_name,
-      c.total_billed_over_capacity,
-      c.total_placed_over_capacity,
-      c.total_distance_traveled,
-      c.total_same_address,
-      c.overall_risk_score
-    FROM combined c
-    LEFT JOIN cusp_audit.demo.provider_attributes pa
-      ON c.provider_licensing_id = pa.provider_licensing_id
-    ORDER BY c.provider_licensing_id;
-  `;
+//         COALESCE(b.total_billed_over_capacity, 0) +
+//         COALESCE(p.total_placed_over_capacity, 0) +
+//         COALESCE(d.total_distance_traveled, 0) +
+//         COALESCE(s.total_same_address, 0) AS overall_risk_score
+//       FROM (
+//         SELECT provider_licensing_id,
+//           SUM(CASE WHEN billed_over_capacity_flag THEN 1 ELSE 0 END) AS total_billed_over_capacity    
+//         FROM cusp_audit.demo.monthly_billed_over_capacity
+//         WHERE YEAR(CAST(StartOfMonth AS DATE)) = ${yearNum}
+//         GROUP BY provider_licensing_id
+//       ) b
+//       FULL OUTER JOIN (
+//         SELECT provider_licensing_id, 
+//           SUM(CASE WHEN placed_over_capacity_flag THEN 1 ELSE 0 END) AS total_placed_over_capacity    
+//         FROM cusp_audit.demo.monthly_placed_over_capacity
+//         WHERE YEAR(CAST(StartOfMonth AS DATE)) = ${yearNum}
+//         GROUP BY provider_licensing_id
+//       ) p
+//         ON b.provider_licensing_id = p.provider_licensing_id
+//       FULL OUTER JOIN (
+//         SELECT provider_licensing_id, 
+//           SUM(CASE WHEN distance_traveled_flag THEN 1 ELSE 0 END) AS total_distance_traveled   
+//         FROM cusp_audit.demo.monthly_distance_traveled
+//         WHERE YEAR(CAST(StartOfMonth AS DATE)) = ${yearNum}
+//         GROUP BY provider_licensing_id
+//       ) d
+//         ON COALESCE(b.provider_licensing_id, p.provider_licensing_id) = d.provider_licensing_id
+//       FULL OUTER JOIN (
+//         SELECT provider_licensing_id, 
+//           SUM(CASE WHEN same_address_flag THEN 1 ELSE 0 END) AS total_same_address      
+//         FROM cusp_audit.demo.monthly_providers_with_same_address
+//         WHERE YEAR(CAST(StartOfMonth AS DATE)) = ${yearNum}
+//         GROUP BY provider_licensing_id
+//       ) s
+//         ON COALESCE(b.provider_licensing_id, p.provider_licensing_id, d.provider_licensing_id) = s.provider_licensing_id
+//     )
+//     SELECT
+//       c.provider_licensing_id,
+//       pa.provider_name,
+//       c.total_billed_over_capacity,
+//       c.total_placed_over_capacity,
+//       c.total_distance_traveled,
+//       c.total_same_address,
+//       c.overall_risk_score
+//     FROM combined c
+//     LEFT JOIN cusp_audit.demo.provider_attributes pa
+//       ON c.provider_licensing_id = pa.provider_licensing_id
+//     ORDER BY c.provider_licensing_id;
+//   `;
 
-  try {
-    const rawData = await queryData(sqlQuery);
-    const result: UiAnnualProviderData[] = rawData.map((item) => {
-      return {
-        provider_licensing_id: item.provider_licensing_id,
-        provider_name: item.provider_name ? item.provider_name : "--",
-        total_billed_over_capacity: item.total_billed_over_capacity || 0,
-        total_placed_over_capacity: item.total_placed_over_capacity || 0,
-        total_distance_traveled: item.total_distance_traveled || 0,
-        total_same_address: item.total_same_address || 0,
-        overall_risk_score: item.overall_risk_score || 0,
-      };
-    });
-  }
-  catch (err: any) {
-    // res.status(500).json({ error: err.message });
-  }
-}
+//   try {
+//     const rawData = await queryData(sqlQuery);
+//     const result: UiAnnualProviderData[] = rawData.map((item) => {
+//       return {
+//         provider_licensing_id: item.provider_licensing_id,
+//         provider_name: item.provider_name ? item.provider_name : "--",
+//         total_billed_over_capacity: item.total_billed_over_capacity || 0,
+//         total_placed_over_capacity: item.total_placed_over_capacity || 0,
+//         total_distance_traveled: item.total_distance_traveled || 0,
+//         total_same_address: item.total_same_address || 0,
+//         overall_risk_score: item.overall_risk_score || 0,
+//       };
+//     });
+//   }
+//   catch (err: any) {
+//     // res.status(500).json({ error: err.message });
+//   }
+// }
