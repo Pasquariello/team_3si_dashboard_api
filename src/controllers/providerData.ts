543065ac60d456f8644fc0641bac7d68adab6f5c
kerj <kerntzj@gmail.com>
2025-11-13 20:24:23 -0800

    move insights api to insight controller, fix city bug

--- src/controllers/providerData.ts
+++ src/controllers/providerData.ts
@@ -557,47 +557,6 @@ export async function getFlaggedCount(req: express.Request, res: express.Respons
   }
 }
 
-// @desc    Update provider insight data - update comment or update flag status
-// @route   put /api/v1/providerData/insights/:id
-// @access  Private
-export async function updateProviderDataInsights(req: express.Request, res: express.Response) {
-  // const body = req.body;
-  const { provider_licensing_id, is_flagged, comment } = req?.body;
-
-  //  id INT,
-  //     row_id  STRING,
-  //     provider_licensing_id INT,
-  //     is_flagged BOOLEAN,
-  //     comment STRING,
-  //     created_at TIMESTAMP
-
-  const sqlQuery = `
-    MERGE INTO cusp_audit.demo.provider_insights target
-    USING (
-      SELECT '${provider_licensing_id}' AS provider_licensing_id,
-      '${is_flagged}' AS is_flagged,
-      '${comment}' AS comment
-    ) source
-    ON target.provider_licensing_id = source.provider_licensing_id
-    WHEN MATCHED THEN
-    UPDATE SET 
-        target.provider_licensing_id = source.provider_licensing_id,
-        target.is_flagged = source.is_flagged,
-        target.comment = source.comment
-    WHEN NOT MATCHED THEN
-    INSERT (provider_licensing_id, is_flagged, comment)
-    VALUES (source.provider_licensing_id, source.is_flagged, source.comment);`;
-
-  //   'select * from cusp_audit.demo limit 10'
-  try {
-    const data = await queryData(sqlQuery);
-    res.json(data);
-  }
-  catch (err: any) {
-    // console.log("err =======", err);
-    res.status(500).json({ error: err.message });
-  }
-}
 
 export async function getProviderAnnualData(req: express.Request, res: express.Response) {
   const yearNum = Number.parseInt(req.params.year, 10);
@@ -647,7 +606,7 @@ export async function getProviderAnnualData(req: express.Request, res: express.R
 // TODO: add Index on city then add SORT BY then remove JS sort here
 export async function getProviderCities(req: express.Request, res: express.Response) {
   const namedParameters = {
-    iLikeCity: `%${req.query.cityName || ""}%`,
+    iLikeCity: `%${req.params.cityName || ""}%`,
   };
 
   const sql = SQL`SELECT DISTINCT city
@@ -655,7 +614,6 @@ export async function getProviderCities(req: express.Request, res: express.Respo
   cusp_audit.fake_data.addresses
   WHERE 1=1
   `;
-
   if (req.query.cityName) {
     sql.append(SQL` AND city ILIKE :iLikeCity`);
   }
